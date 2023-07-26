ffc8d40e715ce14986c65417086603840b0854da
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-07-26 19:01:53 +0200

    Added Daisy UI. other minore front changes

---
+++ resources/js/Components/MainDashboard.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="card card-side bg-base-100 shadow-xl">
+  <figure><img src="https://picsum.photos/200/200" alt="Movie"/></figure>
+  <div class="card-body">
+    <h2 class="card-title">Nome campagna</h2>
+    <p>Descrizione campagna</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Seleziona</button>
+    </div>
+  </div>
+</div>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,24 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import MainDashboard from '@/Components/MainDashboard.vue';
 </script>
 
 <template>
+<div >
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
+                Seleziona campagna
             </h2>
         </template>
 
-        <div class="">
+        <div class="bg-base-200">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="max-w-7xl mx-auto p-6 lg:p-8">
-                    <Welcome />
+                    <MainDashboard />
                 </div>
             </div>
         </div>
     </AppLayout>
+</div>
 </template>
