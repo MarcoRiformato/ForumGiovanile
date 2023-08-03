42d7cbb213592a60e1539734044ce3afe4869f0b
Marco Riformato <marco.riformato@phoops.it>
2023-08-03 10:47:29 +0200

    CreateCampaign frontend reworked

--- resources/js/Pages/CreateCampaign.vue
+++ resources/js/Pages/CreateCampaign.vue
@@ -1,124 +1,99 @@
 <template>
-<div>
-<AppLayout title="Dashboard">
-<div class="bg-base-200">
-    <form class="mx-6 mb-3" @submit.prevent="submit">
-        <div class="form-control grid md:grid-cols-4 gap-4">
-  <!-- Titolo -->
-  <div class="md:col-span-2">
-    <label class="label">
-      <span class="label-text">Titolo</span>
-    </label>
-    <input
-      type="text" 
-      v-model="form.title"
-      placeholder="Inserisci il nome della campagna" 
-      class="input input-bordered w-full input-primary" 
-    />
-  </div>
-
-  <!-- Toggle buttons -->
-  <div class="md:col-span-1 mt-9">
-    <label class="cursor-pointer label">
-      <span class="label-text">Campagna di prova</span>
-      <input
-      v-model="form.testingCampaign"
-        type="checkbox"
-        class="toggle toggle-primary toggle-lg"
-      />
-    </label>
-  </div>
-
-  <div class="md:col-span-1 mt-9">
-    <label class="cursor-pointer label">
-      <span class="label-text">Status</span>
-      <input
-      v-model="form.status"
-        type="checkbox"
-        class="toggle toggle-primary toggle-lg"
-      />
-    </label>
-  </div>
-</div>
-
-
-    <!-- Descrizione -->
-    <div class="form-control">
-      <label class="label">
-        <span class="label-text">Descrizione</span>
-      </label>
-      <textarea
-      v-model="form.description"
-        class="textarea textarea-md textarea-primary w-full h-32" contenteditable
-        placeholder="Descrivi l'campaign">
-      </textarea>
-    </div>
-
-<!-- Link, Date Pickers, and Image Upload -->
-<div class="grid grid-cols-1 md:grid-cols-3 gap-4 items-center mt-4">
-  <div class="form-control">
-    <label class="label">
-      <span class="label-text">Start Date</span>
-    </label>
-    <input
-    v-model="form.startingDate"
-      type="date"
-      class="input input-bordered input-primary w-full"
-    />
-  </div>
-
-  <div class="form-control">
-    <label class="label">
-      <span class="label-text">End Date</span>
-    </label>
-    <input
-    v-model="form.endDate"
-      type="date"
-      class="input input-bordered input-primary w-full"
-    />
-  </div>
-
-  <div class="flex items-center justify-center mt-8">
-    <button type="button" class="btn" onclick="my_modal_1.showModal()">Scegli immagine</button>
-    <dialog id="my_modal_1" class="modal">
-      <form method="dialog" class="modal-box">
-        <ImageUpload/>
-        <div class="modal-action">
-          <button class="btn">Close</button>
+  <AppLayout title="Dashboard">
+    <form class="px-8 py-6 bg-base-200"  @submit.prevent="submit">
+      <div class="space-y-12">
+        <div class="border-b border-white/10 pb-12">
+          <h2 class="text-base font-semibold leading-7 text-white">Crea campagna</h2>
+  
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-4">
+              <label for="username" class="block text-sm font-medium leading-6 text-white">Titolo</label>
+              <div class="mt-2">
+                <div class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                  <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm"> </span>
+                  <input
+                  v-model="form.title"
+                   type="text" class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6" placeholder="Campagna che salverà il mondo" />
+                </div>
+              </div>
+            </div>
+  
+            <div class="col-span-full">
+              <label for="about" class="block text-sm font-medium leading-6 text-white">Descrizione</label>
+              <div class="mt-2">
+                <textarea
+                v-model="form.description"
+                 rows="3" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+              </div>
+              <p class="mt-3 text-sm leading-6 text-gray-400">Descrivi la campagna. Questo testo è solo per uso interno, non verrà visualizzato dagli utenti</p>
+            </div>
+  
+            <div class="col-span-full">
+              <label for="cover-photo" class="block text-sm font-medium leading-6 text-white">Foto</label>
+              <div class="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
+                <div class="text-center">
+                  <div class="mt-4 flex text-sm leading-6 text-gray-400">
+                    <label for="file-upload" class="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500">
+                      <span>Carica una foto</span>
+                      <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                    </label>
+                    <p class="pl-1">oppure trascinala qui</p>
+                  </div>
+                  <p class="text-xs leading-5 text-gray-400">PNG, JPG, GIF up to 10MB</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
-    </dialog>
+      </div>
+  <div class="border-b border-white/10 pb-12">
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-x-6 gap-y-8 justify-items-center items-center sm:justify-items-start">
+      <div class="form-control w-full sm:w-3/4">
+        <label class="label">
+          <span class="label-text">Data di inizio</span>
+        </label>
+        <input
+          v-model="form.startingDate"
+          type="date"
+          class="input input-bordered input-primary w-full"
+        />
+      </div>
+  
+      <div class="form-control w-full sm:w-3/4">
+        <label class="label">
+          <span class="label-text">Data di fine</span>
+        </label>
+        <input
+          v-model="form.endDate"
+          type="date"
+          class="input input-bordered input-primary w-full"
+        />
+      </div>
+    </div>
   </div>
-</div>
-
-<div class="flex justify-center items-center mt-8 space-x-4">
-  <a href="/campaigns" type="button" class="btn btn-secondary w-1/4">Annulla</a>
-  <button type="submit" :disabled="form.processing" class="btn btn-primary w-1/4">Salva</button>
-</div>
-
-
-</form>
-</div>
-</AppLayout>
-</div>
-</template>
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import ImageUpload from '@/Components/ImageUpload.vue';
-import { useForm } from '@inertiajs/vue3';
-
-const form = useForm({
-    title: '',
-    description: '',
-    testingCampaign: '',
-    status: '',
-    startingDate: '',
-    startingDate: ''
-})
-
-const submit = () =>{
-    form.post(route('campaigns.store'));
-}
-
-</script>
-    
+  
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <a href="/campaigns" type="button" class="btn btn-secondary">Annulla</a>
+        <button type="submit" :disabled="form.processing" class="btn btn-primary ">Salva</button>
+      </div>
+    </form>
+  </AppLayout>
+  </template>
+  
+  <script setup>
+  import AppLayout from '@/Layouts/AppLayout.vue';
+  import { useForm } from '@inertiajs/vue3';
+  
+  const form = useForm({
+      title: '',
+      description: '',
+      startingDate: '',
+      endDate: ''
+  })
+  
+  const submit = () =>{
+      form.post(route('campaigns.store'));
+  }
+  
+  </script>
+  
--- resources/js/Pages/ShowCampaign.vue
+++ resources/js/Pages/ShowCampaign.vue
@@ -12,8 +12,8 @@
               <h2 class="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">Dettaglio campagna</h2>
             </div>
             <div class="mt-4 flex md:ml-4 md:mt-0">
-              <a :href="route('campaigns.edit', campaign.id)" type="button" class="inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20">Modifica</a>
-              <button type="button" class="ml-3 inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Pubblica</button>
+              <a :href="route('campaigns.edit', campaign.id)" type="button" class="btn btn-primary me-4">Modifica</a>
+              <button type="button" class="btn btn-secondary">Pubblica</button>
             </div>
           </div>
           <div class="mt-6 border-t border-white/10">
@@ -23,8 +23,13 @@
                   <dd class="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">{{ campaign.title }}</dd>
               </div>
               <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt class="text-sm font-medium leading-6 text-white">Descrizione</dt>
+                  <dd class="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">{{ campaign.description }}</dd>
+              </div>
+              <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                   <dt class="text-sm font-medium leading-6 text-white">Stato</dt>
-                  <dd class="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">{{ campaign.status }}</dd>
+                  <dd v-if="campaign.status == 0" class="mt-1 text-sm text-green-400 leading-6  sm:col-span-2 sm:mt-0">Attivo</dd>
+                  <dd v-if="campaign.status == 1" class="mt-1 text-sm text-yellow-400 leading-6  sm:col-span-2 sm:mt-0">Non attivo</dd>
               </div>
               <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                   <dt class="text-sm font-medium leading-6 text-white">Data inizio</dt>
@@ -33,10 +38,6 @@
               <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                   <dt class="text-sm font-medium leading-6 text-white">Data fine</dt>
                   <dd class="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">{{ campaign.endDate }}</dd>
-              </div>
-              <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt class="text-sm font-medium leading-6 text-white">Descrizione</dt>
-                  <dd class="mt-1 text-sm leading-6  sm:col-span-2 sm:mt-0">{{ campaign.description }}</dd>
               </div>
               </dl>
           </div>
