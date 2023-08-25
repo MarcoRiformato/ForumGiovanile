3a2e9f136f245a96be5250caa9098a7b1ca34207
Marco Riformato <marco.riformato@phoops.it>
2023-08-25 08:24:11 +0200

    Added faker for production

--- resources/js/Pages/Admin/Elections/Index.vue
+++ resources/js/Pages/Admin/Elections/Index.vue
@@ -1,40 +1,56 @@
 <template>
 <AppLayout>
-<div class="bg-base-200 px-8 py-2">
-    <div class="mb-12 flex flex-col sm:flex-row sm:items-center">
-    <div class="flex-auto mb-4 sm:mb-0 mt-8">
-        <h1 class="-mt-8 text-4xl">Gestisci elezioni</h1>
-    </div>
-    <div class=" sm:ml-16 sm:mt-0 sm:flex-none">
-        <button type="button" @click="$inertia.visit(route('admin.elections.create'))" class="btn btn-primary">Crea nuova elezione</button>
-    </div>
-</div>
-    <div class="cards-container -mt-8">
-        <div v-for="election in elections" :key="election.id" class="card-container">
-        <div class="card w-96 bg-secondary text-primary-content">
-            <div class="card-body">
-            <h2 class="card-title">{{ election.name }}</h2>
-            <p>{{ election.description }}</p>
-            <p>{{ election.start_date }}</p>
-            <p>{{ election.end_date }}</p>
-            <p>{{ election.status }}</p>
-            <div class="card-actions justify-end">
-                <button
-                @click.stop="$inertia.visit(route('admin.elections.show', election.id))"
-                 :href="election.link" class="btn">Consulta</button>
-            </div>
-            </div>
+<div class="bg-base-200 px-4 sm:px-6 lg:px-8">
+    <div class="mt-8 flex flex-col sm:flex-row sm:items-center">
+        <div class="flex-auto mb-4 sm:mb-0 mt-8">
+            <h1 class="-mt-8 text-4xl">Gestisci elezioni</h1>
+        </div>
+        <div class="mt-12 sm:ml-16 sm:mt-0 sm:flex-none">
+            <button type="button" @click="$inertia.visit(route('admin.elections.create'))" class="btn btn-primary">Crea nuova elezione</button>
         </div>
     </div>
-</div>
+    <div class="mt-2">
+        <table class="min-w-full divide-y divide-gray-300">
+            <thead>
+                <tr>
+                    <th scope="col" class="py-3.5 px-4 text-left text-sm font-semibold">Nome</th>
+                    <th scope="col" class="py-3.5 px-4 text-left text-sm font-semibold">Descrizione</th>
+                    <th scope="col" class="py-3.5 px-4 text-left text-sm font-semibold">Data Inizio</th>
+                    <th scope="col" class="py-3.5 px-4 text-left text-sm font-semibold">Data Fine</th>
+                    <th scope="col" class="py-3.5 px-4 text-left text-sm font-semibold">Stato</th>
+                    <th scope="col" class="py-3.5 px-4 text-left text-sm font-semibold"></th>
+                </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+                <tr v-for="election in elections" :key="election.id">
+                    <td class="py-4 px-4 text-sm font-medium">{{ election.name }}</td>
+                    <td class="py-4 px-4 text-sm">{{ election.description }}</td>
+                    <td class="py-4 px-4 text-sm">{{ formatDate(election.start_date) }}</td>
+                    <td class="py-4 px-4 text-sm">{{ formatDate(election.end_date) }}</td>
+                    <td class="py-4 px-4 text-sm">{{ election.status }}</td>
+                    <td class="py-4 px-4 text-sm">
+                        <button class="btn btn-secondary" @click.stop="$inertia.visit(route('admin.elections.show', election.id))">Consulta</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 </AppLayout>
 </template>
-<script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
 
-defineProps({
-    elections: Object
-})
+<script setup>
+    import AppLayout from '@/Layouts/AppLayout.vue';
+
+    defineProps({
+        elections: Object
+    })
+
+    const formatDate = (dateString) => {
+    if (!dateString) return "Non definita";
+
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+};
 
 </script>
