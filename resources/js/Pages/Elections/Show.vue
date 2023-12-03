adaffae0ac6d1c2ca708bb2100f1855e8197f09f
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-12-03 14:05:07 +0100

    Remove Candidates text

--- resources/js/Pages/Elections/Show.vue
+++ resources/js/Pages/Elections/Show.vue
@@ -25,7 +25,6 @@
             </div>
           </div>
           <div v-if="question.type === 'candidates'">
-            <h3 class="text-md pb-3">Candidates:</h3>
             <div class="form-control mb-2" v-for="candidate in question.candidates" :key="candidate.id">
               <label class="label cursor-pointer">
                 <span class="label-text">{{ candidate.name }} {{ candidate.description }}</span>
