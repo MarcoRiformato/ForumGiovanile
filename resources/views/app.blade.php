48b6972927ce8fe945788d858c153cfc34333818
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2023-12-07 00:13:08 +0100

    Hotjar implemented

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,18 @@
           gtag('config', 'G-1339JLG5W5');
         </script>
 
+        <!-- Hotjar Tracking Code for Forum -->
+        <script async>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3780678,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+
         <link rel="icon" type="image/png" href="logo_small.png">
 
         <!-- Fonts -->
