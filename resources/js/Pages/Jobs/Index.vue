639d80cedf301cf88e62d2ac3c08e78bf21441a4
Marco Riformato <75418100+MarcoRiformato@users.noreply.github.com>
2024-04-20 22:21:58 +0200

    Adding sondaggi back and putting placeholder in jobs page

--- resources/js/Pages/Jobs/Index.vue
+++ resources/js/Pages/Jobs/Index.vue
@@ -1,37 +1,7 @@
 <template>
 <AppLayout title="LavorElba">
 
-<tr style="color: rgb(221, 218, 213); cursor: pointer; text-align: center; display: table-row; border-radius: 12px; background-color: rgb(89, 79, 0)" id="tr-1">
-  <td style="border-bottom-color: rgb(58, 62, 65); border-bottom-left-radius: 12px; border-bottom-style: solid; border-bottom-width: 2px; border-left-color: rgb(58, 62, 65); border-left-style: solid; border-left-width: 2px; border-top-color: rgb(58, 62, 65); border-top-left-radius: 12px; border-top-style: solid; border-top-width: 2px; padding-left: 7px; padding-right: 7px; vertical-align: middle; display: table-cell; max-width: calc(74px)" id="td-1">
-    <a href="https://remoteok.com/remote-jobs/remote-b2b-saas-sales-lead-uk-eu-kodekloud-714121">
-      <img src="https://remoteok.com/cdn-cgi/image/format=auto,fit=contain,width=100,height=100,quality=50/https://remoteOK.com/assets/img/jobs/9960df763c53cd31ca6b2000bbdc2ed21713245823.png?1713245823" style="background-color: rgb(24, 26, 27); border-right-color: rgb(58, 62, 65); object-fit: scale-down; display: inline-block; width: 60px; height: 60px; margin-right: 0.2em; border-radius: 100%; overflow: hidden" id="img-1" />
-    </a>
-  </td>
-  <td style="border-bottom-color: rgb(58, 62, 65); border-bottom-style: solid; border-bottom-width: 2px; border-top-color: rgb(58, 62, 65); border-top-style: solid; border-top-width: 2px; text-overflow: ellipsis; vertical-align: middle; display: table-cell; height: 5em; max-width: 20em; overflow: hidden; padding: 7px" id="td-2">
-    <a href="https://remoteok.com/remote-jobs/remote-b2b-saas-sales-lead-uk-eu-kodekloud-714121" style="font-size: 18px; font-weight: 800">
-      <h2>B2B SaaS Sales Lead UK &amp; EU</h2>
-    </a>
-    <span style="color: rgb(232, 230, 227); background-color: rgb(21, 166, 102); font-size: 12px; font-weight: 800; padding-bottom: 2px; padding-left: 4px; padding-right: 4px; padding-top: 2px; text-transform: uppercase; display: inline-block; border-radius: 4px">verified</span><br />
-    <span href="https://remoteok.com/kodekloud">
-      <h3 style="font-size: 18px">KodeKloud</h3>
-
-      &nbsp;<img src="https://remoteok.com/assets/hot2x.gif" style="width: 27.9928px; height: 10.9976px" /> </span
-    ><br />
-    <div style="color: rgb(196, 191, 183); background-color: rgb(24, 26, 27); border-left-color: rgb(58, 62, 65); border-right-color: rgb(58, 62, 65); font-size: 12px; padding-bottom: 2px; padding-left: 8px; padding-right: 8px; padding-top: 2px; display: inline-block; max-width: 16em; margin-top: 3.5px; margin-right: 7px; margin-bottom: 3.5px; border-radius: 32px; overflow: hidden">🌏 Worldwide</div>
-    <div style="color: rgb(196, 191, 183); background-color: rgb(24, 26, 27); border-left-color: rgb(58, 62, 65); border-right-color: rgb(58, 62, 65); font-size: 12px; padding-bottom: 2px; padding-left: 8px; padding-right: 8px; padding-top: 2px; display: inline-block; max-width: 16em; margin-top: 3.5px; margin-right: 7px; margin-bottom: 3.5px; border-radius: 32px; overflow: hidden">💰 $60k - $120k</div>
-  </td>
-  <td style="border-bottom-color: rgb(58, 62, 65); border-bottom-style: solid; border-bottom-width: 2px; border-top-color: rgb(58, 62, 65); border-top-style: solid; border-top-width: 2px; min-width: 10em; max-width: 16em; padding: 7px; font-size: 12px; font-weight: 700" id="td-3">
-    <a href="https://remoteok.com/remote-sales-jobs" style="cursor: cell; display: inline-block">
-      <div style="vertical-align: middle; display: inline-block; border-width: 2px; border-color: rgb(134, 124, 110); border-style: solid; border-radius: 12px; margin: 3.5px; padding: 8px">
-        <h3 style="font-weight: 800">Sales</h3>
-      </div>
-    </a>
-  </td>
-  <td style="border-bottom-color: rgb(58, 62, 65); border-bottom-style: solid; border-bottom-width: 2px; border-top-color: rgb(58, 62, 65); border-top-style: solid; border-top-width: 2px; font-size: 14px; vertical-align: middle; display: table-cell; max-width: 6em; padding: 7px" id="td-4">13h</td>
-  <td style="border-bottom-color: rgb(58, 62, 65); border-bottom-right-radius: 12px; border-bottom-style: solid; border-bottom-width: 2px; border-right-color: rgb(58, 62, 65); border-right-style: solid; border-right-width: 2px; border-top-color: rgb(58, 62, 65); border-top-right-radius: 12px; border-top-style: solid; border-top-width: 2px; position: relative; text-align: right; width: 10em; padding: 7px" id="td-5">
-    <a href="https://remoteok.com/l/714121"> </a>
-  </td>
-</tr>
+<p>Lavori in corso</p>
 
 </AppLayout>
 </template>
